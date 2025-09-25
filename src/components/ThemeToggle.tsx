0c8c32c07c39c98e54d8f2d47058ae9614f5d708
C3sar10 <ciperez3@my.canyons.edu>
2025-09-25 12:12:51 -0700

    Merged

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -5,13 +5,8 @@ import { useEffect, useState } from "react";
 import "../styles/globals.css";
 
 export default function ThemeToggle() {
-<<<<<<< HEAD
-    const { resolvedTheme, setTheme } = useTheme();
-    const [mounted, setMounted] = useState(false);
-=======
   const { theme, setTheme, resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
->>>>>>> 2f286ae3f752f9e8f9562ef4f1aa966eb52a683f
 
   useEffect(() => {
     setMounted(true);
@@ -21,21 +16,6 @@ export default function ThemeToggle() {
     }
   }, [theme, resolvedTheme, setTheme]);
 
-<<<<<<< HEAD
-    const isDark = resolvedTheme === "dark";
-    console.log(resolvedTheme);
-    
-
-    return (
-        <button
-            onClick={() => setTheme(isDark ? "light" : "dark")}
-            className="p-2 rounded bg-[var(--primary-button)] transition"
-        >
-            {isDark ? "Switch to Light Mode" : "Switch to Dark Mode"}
-        </button>
-    );
-}
-=======
   if (!mounted) return null;
 
   return (
@@ -47,4 +27,3 @@ export default function ThemeToggle() {
     </button>
   );
 }
->>>>>>> 2f286ae3f752f9e8f9562ef4f1aa966eb52a683f
