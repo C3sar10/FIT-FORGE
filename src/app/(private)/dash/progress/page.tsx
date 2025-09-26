9e90a4765beb168ff8614646ada523ba72113055
C3sar10 <ciperez3@my.canyons.edu>
2025-09-26 14:47:43 -0700

    Updates

--- src/app/(private)/dash/progress/page.tsx
+++ src/app/(private)/dash/progress/page.tsx
@@ -1,9 +1,38 @@
+"use client";
 import React from "react";
+
+import { Bar, BarChart, CartesianGrid, XAxis, Tooltip, ResponsiveContainer, Label } from "recharts"
 
 type Props = {};
 
+const data = [
+  { name: "Jan", value: 400 },
+  { name: "Feb", value: 300 },
+  { name: "Mar", value: 500 },
+  { name: "Apr", value: 200 },
+]
+
 const page = (props: Props) => {
-  return <div>page</div>;
+  return (
+    <div className="w-full h-full flex flex-col items-center">
+      <div className="w-full p-4 flex items-center justify-between">
+        <p className="text-sm md:text-base font-medium">Progress</p>
+        <p className="text-sm md:text-base font-medium">Settings</p>
+      </div>
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <ResponsiveContainer width={"90%"} height={300}>
+          <BarChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name">
+              <Label value="Months" offset={-5} position="insideBottom" className="text-white"/>
+            </XAxis>
+            <Tooltip />
+            <Bar dataKey="value" fill="#6366f1" /> {/* Indigo-500 */}
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  )
 };
 
 export default page;
